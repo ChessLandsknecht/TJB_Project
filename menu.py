763964b155ec004637f41ffe7ddd73d0d466a5a7
ChessLandsknecht <ahpeppernsalt@gmail.com>
2023-11-09 22:32:43 +1100

    First Commit

--- menu.py
+++ menu.py
@@ -1,1 +1,13 @@
-import streamlit
+import streamlit as st
+
+st.title("Streamlit Program")
+
+firstTextInput = st.text_input("First textbox",key= 1)
+secondTextInput = st.text_input("Second textbox",key=2 )
+
+button = st.button("Save text")
+if button:
+    firstDisplay = st.write(firstTextInput)
+    secondDisplay = st.write(secondTextInput)
+
+    
